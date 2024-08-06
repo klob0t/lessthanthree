82e8891a875ee74801c7341cf92f9f66a0e8fef7
klob0t <155458576+klob0t@users.noreply.github.com>
2024-08-06 03:10:30 +0000

    add fiber & drei

---
+++ src/app/components/Rose.jsx
@@ -0,0 +1,12 @@
+'use client'
+import { useRef } from "react"
+import { Canvas, useFrame, useLoader } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { Mesh } from "three"
+
+export function Rose () {
+    <Canvas>
+
+    </Canvas>
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
+import Rose from './components/Rose'
 
 export default function Home() {
   return (
